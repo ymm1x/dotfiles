8c36d139f61305aa68f67cfcde512d338e1d020a
ymm1x <biztk28@gmail.com>
2018-01-08 21:33:32 +0900

    add .editorconfig and reformat codes

--- setup.sh
+++ setup.sh
@@ -5,15 +5,14 @@ source lib/echos.sh
 
 readonly DOT_FILES=( .vimrc )
 
-info "Creating dotfile symlinks..."
 for file in ${DOT_FILES}; do
-    dest=${HOME}/${file}
-    if [ -e ${dest} ]; then
-        fail "[warn] ${dest}: skipped (already exists)"
-    else
-        ln -s $HOME/dotfiles/$file $dest
-        ok "[ ok ] ${dest}: created"
-    fi
+  dest=${HOME}/${file}
+  if [ -e ${dest} ]; then
+    fail "[warn] ${dest}: skipped (already exists)"
+  else
+    ln -s $HOME/dotfiles/$file $dest
+    ok "[ ok ] ${dest}: created"
+  fi
 done
 
 ok "All installed!"
